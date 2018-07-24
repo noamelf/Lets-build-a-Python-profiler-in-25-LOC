0ae4221b3e73fb83868664eac2e8aeafdc8735d2
Noam Elfanbaum <noam.elf@gmail.com>
2018-07-24 23:04:53 +0100

    test

--- src/sfProfiler.py
+++ src/sfProfiler.py
@@ -1,16 +1,22 @@
-import atexit, collections, signal, traceback
+import atexit
+import collections
+import signal
+import traceback
 
 stats = collections.defaultdict(int)
+
 
 def _sample(_, call_stack):
     stack = traceback.extract_stack(call_stack)
     formatted_stack = ';'.join(line.line for line in stack)
     stats[formatted_stack] += 1
 
+
 def start(interval=0.005):
     signal.signal(signal.SIGPROF, _sample)
     signal.setitimer(signal.ITIMER_PROF, interval, interval)
     atexit.register(lambda: signal.setitimer(signal.ITIMER_PROF, 0))
+
 
 def format_stats():
     stack_count = [f'{stack} {count}' for stack, count in stats.items()]
