bd3b5dd49d79a658fa924e39e144f39b6dc9dd96
Noam Elfanbaum <noam.elf@gmail.com>
2018-07-24 05:20:35 +0300

    test

--- src/setprofile.py
+++ src/setprofile.py
@@ -3,7 +3,7 @@ import sys
 import traceback
 
 def profiler(call_stack, event, arg):
-    if event == 'c_call':
+    # if event == 'c_call':
         line = traceback.extract_stack(call_stack)[0]
         print(f'Function is: {arg}')
         print(f'Line executed is: {line.line}')
