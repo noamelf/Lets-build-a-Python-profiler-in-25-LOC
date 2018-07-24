9a655cc138d61e3cc7351d922e6daf2dc8111f7e
Noam Elfanbaum <noam.elf@gmail.com>
2018-07-24 23:23:59 +0100

    test

--- src/sfProfiler.py
+++ src/sfProfiler.py
@@ -3,8 +3,6 @@ import collections
 import signal
 import traceback
 
-INTERVAL = 0.005
-
 stats = collections.defaultdict(int)
 
 
@@ -14,12 +12,14 @@ def _sample(_, call_stack):
     stats[formatted_stack] += 1
 
 
-def start(interval=INTERVAL):
+def start(interval=0.005):
     signal.signal(signal.SIGPROF, _sample)
     signal.setitimer(signal.ITIMER_PROF, interval, interval)
-    atexit.register(lambda: signal.setitimer(signal.ITIMER_PROF, 0))
+    atexit.register(lambda: signal.setitimer(
+        signal.ITIMER_PROF, 0))
 
 
 def format_stats():
-    stats_out = [f'{trace} {count}' for trace, count in stats.items()]
+    stats_out = [f'{trace} {count}' for trace, count
+                 in stats.items()]
     return '\n'.join(stats_out)
