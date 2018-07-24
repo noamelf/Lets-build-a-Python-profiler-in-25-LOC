cc7465ee3ba78d4effc2fef55bde26cae15606c1
Noam Elfanbaum <noam.elf@gmail.com>
2018-07-24 04:59:34 +0300

    test

--- src/stack_access.py
+++ src/stack_access.py
@@ -6,7 +6,8 @@ import traceback
 def show_stack():
     for _, call_stack in sys._current_frames().items():
         for frame in traceback.extract_stack(call_stack):
-            print(f'{frame.filename}:{frame.lineno}:{frame.name} - "{frame.line}"')
+            print(f'{frame.filename}:{frame.lineno}:'
+                  f'{frame.name} - "{frame.line}"')
 
 
 def bar():
