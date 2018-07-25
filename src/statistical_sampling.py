65d9144f86a0ddb7cada4e6ec2efe23226b83a85
Noam Elfanbaum <noam.elf@gmail.com>
2018-07-25 09:57:38 +0100

    test

--- src/statistical_sampling.py
+++ src/statistical_sampling.py
@@ -8,7 +8,8 @@ def print_handler(signum, call_stack):
 def start(handler, interval=1):
     signal.signal(signal.SIGPROF, handler)
     signal.setitimer(signal.ITIMER_PROF, interval, interval)
-    atexit.register(lambda: signal.setitimer(signal.ITIMER_PROF, 0))
+    atexit.register(lambda: signal.setitimer(
+        signal.ITIMER_PROF, 0))
 
 start(print_handler)
 result = 7 ** 7 ** 7
